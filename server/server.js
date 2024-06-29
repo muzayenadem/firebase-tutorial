63b52ce4da6239a5cf18a3b137c2f0addaa727e5
muzayenadem <muzynadem@gmail.com>
2024-06-30 02:41:31 +0300

    some chanege at server.js file

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 // API Routes
 //app.use(router);
 app.get('/',(req,res)=>{
-    res.send('Hello its me')
+    res.json('Hello its me')
 })
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
