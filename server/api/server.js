f91422abe5019401a0e0be648802e5110f6d1b20
muzayenadem <muzynadem@gmail.com>
2024-06-30 10:55:08 +0300

    on render

--- server/api/server.js
+++ server/api/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-//const router = require('./routes/api')
+const router = require('../routes/api')
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 // Test Route
 // API Routes
-//app.use(router);
+app.use(router);
 app.get('/',(req,res)=>{
     res.json('Hello its me')
 })
