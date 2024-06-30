f9cbff37b40c2d5e4fbad8ef926f8ae110f885d8
muzayenadem <muzynadem@gmail.com>
2024-06-30 13:30:26 +0300

    something

--- server/public/server.js
+++ server/public/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const router = require('../routes/api')
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 // Middleware
 app.use(cors());
